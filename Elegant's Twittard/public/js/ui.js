0ea41f356ed12c07daa2655be18a018b46034531
GantGanz <anggerelegant7@gmail.com>
2020-11-26 14:10:42 +0700

    26Nov20

--- Elegant's Twittard/public/js/ui.js
+++ Elegant's Twittard/public/js/ui.js
@@ -1,3 +1,18 @@
+function togglePopupMessage(message) {
+    document.getElementById("popup-message").classList.toggle("active");
+    document.getElementById("message").innerHTML = message;
+}
+
+function togglePopupMeme(meme) {
+    document.getElementById("popup-meme").classList.toggle("active");
+    document.getElementById("meme").innerHTML = `<img src="../img/contents/` + meme + `" alt="` + meme + `" loading="lazy">`;
+}
+
+function togglePopupMemeO(meme) {
+    document.getElementById("popup-meme").classList.toggle("active");
+    document.getElementById("meme").innerHTML = `<img src="img/contents/` + meme + `" alt="` + meme + `" loading="lazy">`;
+}
+
 // render tweet data
 // var countComments = 0;
 const renderComments = (data) => {
